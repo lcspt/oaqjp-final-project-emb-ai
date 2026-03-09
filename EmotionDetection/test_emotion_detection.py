8f04f78bf27cd9abde4a56b0219fd369652cad57
lcspt <lupo@xs4all.nl>
2026-03-09 08:11:22 -0400

    Rename class for EmotionDetection unit test

--- EmotionDetection/test_emotion_detection.py
+++ EmotionDetection/test_emotion_detection.py
@@ -1,7 +1,7 @@
 import unittest
 from EmotionDetection.emotion_detection import emotion_detector
 
-class TestSentimentAnalyzer(unittest.TestCase): 
+class TestEmotionDetector(unittest.TestCase): 
 
     def test_emotion_detector(self):
         self.assertEqual(emotion_detector('I am glad this happened')['dominant_emotion'], 'joy')
