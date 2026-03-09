ed0bae8b2ff4e4ad0f89060d7b42676414707873
lcspt <lupo@xs4all.nl>
2026-03-09 09:22:33 -0400

    handle 400 status in call to watson
    no input supplied by user

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,7 +8,15 @@ def emotion_detector(text_to_analyse):
     Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     Input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(URL, json = Input_json, headers=Headers)
-    data = response.json()
+    
+    if response.status_code == 400:
+        print(response.status_code)
+        data = {'emotionPredictions': 
+        [{'emotion': {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None}}]}
+    else:
+        data = response.json()
+    
+    print(data)
 
     emotions = data['emotionPredictions'][0]['emotion']
     ranked = sorted(emotions.items(), key=lambda x: x[1], reverse=True)
