36f9e74999522a81342358c53d24aaeb5c521eab
lcspt <lupo@xs4all.nl>
2026-03-09 08:47:16 -0400

    include some newlines in emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,13 +3,17 @@ import requests # Import the requests library to handle HTTP requests
 import json
 
 def emotion_detector(text_to_analyse):
+
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     Input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(URL, json = Input_json, headers=Headers)
     data = response.json()
+
     emotions = data['emotionPredictions'][0]['emotion']
     ranked = sorted(emotions.items(), key=lambda x: x[1], reverse=True)
+
     sorted_emotions = dict(ranked)
     sorted_emotions['dominant_emotion'] = ranked[0][0]
+
     return sorted_emotions
